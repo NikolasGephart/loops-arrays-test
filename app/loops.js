18ff2166fa3725de31623e9bb7f898403c7538f0
ChyaBoiNik <nikolas.gephart@gmail.com>
2021-06-29 16:53:54 -0600

    struggle bus

--- app/loops.js
+++ app/loops.js
@@ -4,9 +4,12 @@
 // input: 4
 // output: 24
 // 1*2*3*4 = 24
-
+//TODO come back here
 function factorials(num) {
+  for (let i = 1; i <= num; i++) {
+    const element = array[i];
 
+  }
 }
 
 // ------------------------------------------
@@ -15,8 +18,26 @@ function factorials(num) {
 // Example:
 // input: 2, 6
 // output: 20
+/*
+ STUB find whats the lowest and whats the highest num. Find how many numbers are between the two (ex. 6-2 = 4) 
+ Then take the largest number + itself minus 1 until the "addition variable" is equal to the lowest number
+ */
 
+//range(2, 5)
+//gap = 5-2
+// let index = 2; 2<=3; 3--)
 function rangeTotal(n1, n2) {
+
+
+
+  for (let index = 0; index == gap; index++) {
+    let gap = n2 - n1
+    init = n2 - 1
+    let total = n1 + n2 + init
+
+  }
+
+
 
 }
 
@@ -24,7 +45,7 @@ function rangeTotal(n1, n2) {
 
 
 // 3. Write a function that will take in a starting number, ending number, and a divisor. Iterate
-// over the range of the numbers and rtuen how many numbers can be divided by the divisor.
+// over the range of the numbers and return how many numbers can be divided by the divisor.
 // Example:
 // input: 15, 20, 5
 // output: 2
